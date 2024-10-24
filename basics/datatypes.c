403f159126635a5b36fbf186642c349fbfed3b3b
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2024-10-24 14:31:11 +0530

    Add Basic Data Types

---
+++ basics/datatypes.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    // Basic Data Types
+    int age = 40;
+    double gpa = 3.6;
+    char grade = 'A';
+
+    // String 
+    char phrase[] = "Giraffe Academy";
+
+    return 0;
+}
