6cb5557d66afc9ec6f183adf79437eacd0f8c909
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2024-10-24 14:36:44 +0530

    format specifier and printf

---
+++ basics/printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    // Format specifier
+    int favNum = 90;
+    printf("%d", favNum);
+    printf("My favorite %s is %f", "number", 500.434);
+
+    return 0;
+}
